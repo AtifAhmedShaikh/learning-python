39268efd1c2c43ef53e8f17f55a061b32a4c0d56
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-04-17 05:22:57 -0700

    basic python setup and some testig routes

---
+++ server/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+# from pydantic import BaseModel
+
+
+app = FastAPI()
+#mongodb+srv://atifahmed19:myJsCode19@cluster0.ytfhno1.mongodb.net/snapNewsData?retryWrites=true&w=majority 
+
+# MongoDB Connection String
+databaseConnectionURL = "mongodb+srv://atifahmed19:myJsCode19@cluster0.ytfhno1.mongodb.net/snapNewsData?retryWrites=true&w=majority "
+
+# # Connect to MongoDB
+# client = MongoClient(databaseConnectionURL)
+# db = client['my_python_database']
+# collection = db['my_collection']
+try:
+  client = MongoClient(databaseConnectionURL)
+  db = client['my_python_database']
+  collection = db['my_collection']
+  print("Database has Connected successfully ")
+except Exception as error:
+  print("Error while connecting database")  
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/test-route")
+def test_route():
+    obj={"message":"My Testing route is here !"}
+    return obj
+
+@app.get("/test-data")
+def test_route():
+    my_list = ["apple", "banana", "cherry"]
+    obj = {"data": my_list, "message": "My Testing data route is here!"}
+    return obj
+  
+
+@app.get("/test-params/{my_id}")
+def test_params(my_id: str):
+  print(my_id)
+  return {"message":"Testing params"}
+
+# class Todo(BaseModel):
+#     name: str
+#     age: int
+#     email: str
+
+
+# @app.get("/create-todo")
+# def create_todo():
